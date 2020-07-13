b12703c8db9acfa0010dca39877fb518e1961a6a
Bruno Campuzano <39270874+BrunnoCD@users.noreply.github.com>
2020-07-13 15:53:38 -0500

    Update prueba2.c
    seems to work

--- prueba2.c
+++ prueba2.c
@@ -3,7 +3,6 @@ hola, estamos probando los comandos de github... veamos que pasa
 
 esta es la segunda ocasion que modifico este archivo.s
 
-terdera vez, ahora usando - m "nombre" en el commit
-s
+tercera vez, ahora usando - m "nombre" en el commit
 
 just do it.
