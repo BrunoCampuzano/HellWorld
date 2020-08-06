a45045498028848206c4a36d33184210552517ea
Bruno Campuzano <39270874+BrunnoCD@users.noreply.github.com>
2020-08-06 18:02:35 -0500

    Update roman_numerals.c
    tidying up

--- roman-numerals/src/roman_numerals.c
+++ roman-numerals/src/roman_numerals.c
@@ -1,56 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "roman_numerals.h"
 #include <string.h>
-//struct {
-//   int key;
-//   char *romanNumeralText;
-//}romanNumeralDictionary[] = {
-//   {1, "I"},
-//   {5, "V"},
-//   {10, "X"}
-//};
+#include "roman_numerals.h"
 
-typedef struct {
-	int key;
-	char *romanNumeralText;
+enum
+{
+  ARandomCharacterSize = 45	
+}
+
+typedef struct
+{
+   int key;
+   char *romanNumeralText;
 } RomanNumeralDictionary_t;
 
-RomanNumeralDictionary_t RomanNumeralDictionary[] = { // Used to look up for common values
-		{ 1000, "M" },
-		{ 900, "CM" },
-		{ 500, "D" },
-		{ 400, "CD" },
-		{ 100, "C" },
-		{ 90, "XC" },
-		{ 50, "L" },
-		{ 40, "XL" },
-		{ 10, "X" },
-		{ 9, "IX" },
-		{ 5, "V" },
-		{ 4, "IV" },
-		{ 1, "I" }
-
+RomanNumeralDictionary_t dictionary[] = {
+   { 1000, "M" },
+   { 900, "CM" },
+   { 500, "D" },
+   { 400, "CD" },
+   { 100, "C" },
+   { 90, "XC" },
+   { 50, "L" },
+   { 40, "XL" },
+   { 10, "X" },
+   { 9, "IX" },
+   { 5, "V" },
+   { 4, "IV" },
+   { 1, "I" }
 };
 
-char *to_roman_numeral(int number) {
-	char *romanNumeralResult = malloc(sizeof(char) * 45); // Get the number sent and allocate in memory in order to use
-	int dictionaryIndex = 0; // USED to index into the array
-	sprintf(romanNumeralResult, ""); /*The sprintf function returns the number of characters stored in the array s,
-	not including the terminating null character. */
+char *to_roman_numeral(int number)
+{
+   char *romanNumeralResult = malloc(sizeof(char) * ARandomCharacterSize);
+   int dictionaryIndex = 0;
+   sprintf(romanNumeralResult, "");
 
-//	for (int i = 0; i<=number; i++) {
-	while (number > 0) {
+   while(number > 0)
+   {
+      if(number >= dictionary[dictionaryIndex].key)
+      {
+         sprintf(romanNumeralResult, "%s%s", romanNumeralResult, dictionary[dictionaryIndex].romanNumeralText);
 
-		if (number >= RomanNumeralDictionary[dictionaryIndex].key) { // if received value is equal or bigger
-			sprintf(romanNumeralResult,"%s%s", romanNumeralResult,RomanNumeralDictionary[dictionaryIndex].romanNumeralText);
+         number = number - dictionary[dictionaryIndex].key;
+      }
+      else
+      {
+         dictionaryIndex++;
+      }
+   }
 
-			number = number - RomanNumeralDictionary[dictionaryIndex].key; // substract the current value
-		}
-		else {
-			dictionaryIndex++;
-		}
-	}
-
-	return romanNumeralResult;
+   return romanNumeralResult;
 }
